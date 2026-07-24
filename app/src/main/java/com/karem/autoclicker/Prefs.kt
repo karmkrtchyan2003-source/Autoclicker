775ec48f057dd56d713f23b99350730ec14d8b0c
karmkrtchyan2003-source <karmkrtchyan2003@gmail.com>
2026-07-25 01:16:43 +0200

    Create Prefs.kt

---
+++ app/src/main/java/com/karem/autoclicker/Prefs.kt
@@ -0,0 +1,10 @@
+package com.karem.autoclicker
+
+object Prefs {
+    const val NAME = "autoclicker_prefs"
+    const val INTERVAL_MS = "interval_ms"
+    const val TARGET_X = "target_x"
+    const val TARGET_Y = "target_y"
+    const val TRIGGER_X = "trigger_x"
+    const val TRIGGER_Y = "trigger_y"
+}
