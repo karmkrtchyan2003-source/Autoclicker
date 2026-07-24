85bb5ae255f333ccd4c196a34bcc89d7c911553d
karmkrtchyan2003-source <karmkrtchyan2003@gmail.com>
2026-07-25 01:17:10 +0200

    Create ClickAccessibilityService.kt

---
+++ app/src/main/java/com/karem/autoclicker/ClickAccessibilityService.kt
@@ -0,0 +1,38 @@
+package com.karem.autoclicker
+
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+import android.view.accessibility.AccessibilityEvent
+
+class ClickAccessibilityService : AccessibilityService() {
+
+    companion object {
+        var instance: ClickAccessibilityService? = null
+            private set
+    }
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        instance = this
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (instance === this) instance = null
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        // Не используется - нужен только dispatchGesture.
+    }
+
+    override fun onInterrupt() {}
+
+    fun performClick(x: Float, y: Float) {
+        val path = Path()
+        path.moveTo(x, y)
+        val stroke = GestureDescription.StrokeDescription(path, 0, 1)
+        val gesture = GestureDescription.Builder().addStroke(stroke).build()
+        dispatchGesture(gesture, null, null)
+    }
+}
